c85de747eee729e9845c7601e497d236173a4937
kwanghyunpark <akaii@kw.ac.kr>
2016-01-10 16:27:08 +0900

    Update extension.js

--- scratch/extension.js
+++ scratch/extension.js
@@ -1,5 +1,5 @@
 (function(ext) {
-    // Cleanup function when the extension is unloaded
+    var device = null;
     ext._shutdown = function() {};
 
     // Status reporting code
@@ -7,6 +7,49 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
+    
+
+	var poller = null;
+function deviceOpened(dev) {
+    // if device fails to open, forget about it
+    if (dev == null) device = null;
+
+    // otherwise start polling
+    poller = setInterval(function() {
+        rawData = device.read();
+    }, 20);
+};
+ext._deviceConnected = function(dev) {
+    if(device) return;
+
+    device = dev;
+    device.open(deviceOpened);
+};
+
+ext._deviceRemoved = function(dev) {
+    if(device != dev) return;
+    if(poller) poller = clearInterval(poller);
+    device = null;
+};
+
+    var potentialDevices = [];
+ext._deviceConnected = function(dev) {
+    potentialDevices.push(dev);
+
+    if (!device) {
+        tryNextDevice();
+    }
+}
+
+    var paramString = window.location.search.replace(/^\?|\/$/g, '');
+    var vars = paramString.split('&');
+    var lang = 'en';
+    for(var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if(pair.length > 1 && pair[0] == 'lang')
+            lang = pair[1];
+    }
+    alert(lang);
 
     // Block and block menu descriptions
     var descriptor = {
@@ -29,5 +72,6 @@
     };
 
     // Register the extension
-    ScratchExtensions.register('Hamster', descriptor, ext);
+    var serial_info = {type: 'serial'};
+    ScratchExtensions.register('Hamster', descriptor, ext, serial_info);
 })({});
