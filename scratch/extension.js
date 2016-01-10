8880a0e7538e37efda142533eec86b9ae9bf70bf
kwanghyunpark <akaii@kw.ac.kr>
2016-01-10 17:32:32 +0900

    Update extension.js

--- scratch/extension.js
+++ scratch/extension.js
@@ -61,6 +61,7 @@ function deviceOpened(dev) {
         dev.open({ stopBits: 0, bitRate: 115200, ctsFlowControl: 2 });
         console.log('Attempting connection with ' + dev.id);
         dev.set_receive_handler(function(data) {
+        	console.log(data);
             var inputData = new Uint8Array(data);
             //processInput(inputData);
         });
