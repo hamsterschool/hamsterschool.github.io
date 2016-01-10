47a207cdd391c1090303a5a9e345d4964589101a
kwanghyunpark <akaii@kw.ac.kr>
2016-01-10 17:22:24 +0900

    Update extension.js

--- scratch/extension.js
+++ scratch/extension.js
@@ -48,9 +48,8 @@ function deviceOpened(dev) {
         console.log('connected');
         //if(device) return;
         potentialDevices.push(dev);
-        console.log(dev);
-        //if(!device)
-        //    tryNextDevice();
+        if(!device)
+            tryNextDevice();
     };
 
     var poller = null;
