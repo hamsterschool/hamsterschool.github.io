eeee605d58dfbabd514522faef4b6d4f2e498adb
kwanghyunpark <akaii@kw.ac.kr>
2016-01-21 16:11:50 +0900

    Update extension.js

--- scratch/extension.js
+++ scratch/extension.js
@@ -46,24 +46,17 @@ JsonHandler.prototype.write = function(key, value) {
 	return false;
 };
 
-ext.open = function(id, url) {
-    if('WebSocket' in window) {
-        try {
-            var socket = new WebSocket(url);
-            socket.binaryType = 'arraybuffer';
-            ext.socket = socket;
-            socket.onopen = function() {
-                socket.onmessage = function(message) { // message: MessageEvent
-                    var data = message.data;
-                    if(!ext.receiveHandler) {
-                        ext.receiveHandler = new JsonHandler(id);
-                    }
-                    if(ext.receiveHandler) {
-                        ext.receiveHandler.decode(data);
-                        if(ext.dataChangedListener) {
-                            ext.dataChangedListener();
-                        }
-                    }
+	if('WebSocket' in window) {
+		try {
+			var socket = new WebSocket(url);
+			socket.binaryType = 'arraybuffer';
+			ext.socket = socket;
+			socket.onopen = function() {
+				socket.onmessage = function(message) { // message: MessageEvent
+					try {
+						ext.receiveData = JSON.parse(message.data);
+					} catch (e) {
+					}
                 };
                 socket.onclose = function() {
                     if(ext.stateChangedListener) {
@@ -106,45 +99,88 @@ ext.open('020401', 'ws://localhost:23518');
 //    device = null;
 //};
 
+	ext.leftProximity = function() {
+		ext.receiveData.leftProximity;
+	}
 
-
-    var langs = {
-        'ko': 'ko'
-    };
     var vars = window.location.search.replace(/^\?|\/$/g, '').split("&");
-    var lang = undefined;
+    var lang = 'en';
+    var level = '3';
+    var board = 'n';
+    var pair;
     for(var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if(pair.length > 1 && pair[0] == 'lang')
-            lang = pair[1];
+        pair = vars[i].split('=');
+        if(pair.length > 1) {
+            if(pair[0] == 'lang')
+                lang = pair[1];
+            else if(pair[0] == 'level')
+                level = pair[1];
+            else if(pair[0] == 'board')
+                board = pair[1];
+        }
     }
-    if(!lang)
-        lang = window.navigator.userLanguage || window.navigator.language;
-    lang = langs[lang];
-    if(lang == undefined)
-        lang = 'en';
-    //alert(lang);
 
-    // Block and block menu descriptions
+	var blocks = {
+		en3n: [
+			["w", "move forward for %n secs", "moveForwardForSecs", "1"],
+			["w", "move backward for %n secs", "moveBackwardForSecs", "1"],
+			["w", "turn %m.left_right for %n secs", "turnForSecs", "left", "1"],
+			[" ", "change wheels by left: %n right: %n", "changeBothWheelsBy", "10", "10"],
+			[" ", "set wheels to left: %n right: %n", "setBothWheelsTo", "30", "30"],
+			[" ", "change %m.left_right_both wheel by %n", "changeWheelBy", "left", "10"],
+			[" ", "set %m.left_right_both wheel to %n", "setWheelTo", "left", "30"],
+			[" ", "follow %m.black_white line using %m.left_right_both floor sensor", "followLineUsingFloorSensor", "black", "left"],
+			["w", "follow %m.black_white line until %m.left_right_front_rear intersection", "followLineUntilIntersection", "black", "left"],
+			[" ", "set following speed to %m.speed", "setFollowingSpeedTo", "5"],
+			[" ", "stop", "stop"],
+			[" ", "set %m.left_right_both led to %m.color", "setLedTo", "left", "red"],
+			[" ", "clear %m.left_right_both led", "clearLed", "left"],
+			["w", "beep", "beep"],
+			[" ", "change buzzer by %n", "changeBuzzerBy", "10"],
+			[" ", "set buzzer to %n", "setBuzzerTo", "1000"],
+			[" ", "clear buzzer", "clearBuzzer"],
+			["w", "play note %m.note %m.octave for %n beats", "playNoteFor", "C", "4", "0.5"],
+			["w", "rest for %n beats", "restFor", "0.25"],
+			[" ", "change tempo by %n", "changeTempoBy", "20"],
+			[" ", "set tempo to %n bpm", "setTempoTo", "60"],
+			["r", "left proximity", "leftProximity"],
+			["r", "right proximity", "rightProximity"],
+			["r", "left floor", "leftFloor"],
+			["r", "right floor", "rightFloor"],
+			["r", "x acceleration", "accelerationX"],
+			["r", "y acceleration", "accelerationY"],
+			["r", "z acceleration", "accelerationZ"],
+			["r", "light", "light"],
+			["r", "temperature", "temperature"],
+			["r", "signal strength", "signalStrength"],
+			["b", "hand found?", "handFound"],
+			[" ", "set port %m.port to %m.mode", "setPortTo", "A", "analog input"],
+			[" ", "change output %m.port by %n", "changeOutputBy", "A", "10"],
+			[" ", "set output %m.port to %n", "setOutputTo", "A", "100"],
+			["r", "input A", "inputA"],
+			["r", "input B", "inputB"]
+		]
+	};
+	var menus = {
+		en: {
+			"left_right": ["left", "right"],
+			"left_right_both": ["left", "right", "both"],
+			"black_white": ["black", "white"],
+			"left_right_front_rear": ["left", "right", "front", "rear"],
+			"speed": ["1", "2", "3", "4", "5", "6", "7", "8"],
+			"color": ["red", "yellow", "green", "cyan", "blue", "magenta", "white"],
+			"note": ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B"],
+			"octave": ["1", "2", "3", "4", "5", "6", "7"],
+			"port": ["A", "B", "A and B"],
+			"mode": ["analog input", "digital input", "servo output", "pwm output", "diginal output"]
+		}
+	};
+    
     var descriptor = {
-        blocks: [
-            // Block type, block name, function name
-            ["w", "move forward", "moveForward"],
-		    ["w", "move backward", "moveBackward"],
-		    ["w", "turn %m.left_right", "turn", "left"],
-		    [" ", "set %m.left_right_both led to %m.color", "setLedTo", "left", "red"],
-		    [" ", "clear %m.left_right_both led", "clearLed", "left"],
-		    ["w", "beep", "beep"],
-		    ["b", "hand found?", "handFound"]
-        ],
-        menus: {
-		    "left_right": ["left", "right"],
-		    "left_right_both": ["left", "right", "both"],
-		    "color": ["red", "yellow", "green", "cyan", "blue", "magenta", "white"]
-	    },
+        blocks: blocks[lang + level + board],
+        menus: menus[lang],
 	    url: "http://hamster.school"
     };
 
-    // Register the extension
     ScratchExtensions.register('Hamster', descriptor, ext);
 })({});
