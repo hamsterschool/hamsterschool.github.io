9525b69d298f7ade60a507870e0c5c88fba1e36b
kwanghyunpark <akaii@kw.ac.kr>
2017-10-07 13:15:42 +0900

    Update turtle161.js

--- scratchx/turtle161.js
+++ scratchx/turtle161.js
@@ -44,8 +44,9 @@
 		motionRadius: 0
  	};
 	var packet = {
-		turtle: motoring
-	};
+		version: 1,
+		robot: motoring
+ 	};
 	var connectionState = 1;
 	var pulseCallback = undefined;
 	var soundId = 0;
@@ -663,17 +664,30 @@
 				sock.binaryType = 'arraybuffer';
 				socket = sock;
 				sock.onopen = function() {
-					sock.onmessage = function(message) { // message: MessageEvent
+					var slaveVersion = 1;
+					sock.onmessage = function(message) {
 						try {
-							var data = JSON.parse(message.data);
-							if(data.module == 'turtle') {
-								if(data.type == 1) {
-									if(data.index == 0) {
-										sensory = data;
+							var received = JSON.parse(message.data);
+							slaveVersion = received.version || 0;
+							if(received.type == 0) {
+								if(received.module == 'turtle') {
+									connectionState = received.state;
+								}
+							} else {
+								if(slaveVersion == 1) {
+									var data;
+									for(var i in received) {
+										data = received[i];
+										if(data.module == 'turtle' && data.index == 0) {
+											sensory = data;
+											handleSensory();
+										}
+									}
+								} else {
+									if(received.module == 'turtle' && received.index == 0) {
+										sensory = received;
 										handleSensory();
 									}
-								} else if(data.type == 0) {
-									connectionState = data.state;
 								}
 							}
 						} catch (e) {
