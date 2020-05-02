b6d1b03b0e64f2861f0527589b723c3ad81e40b1
kwanghyunpark <akaii@kw.ac.kr>
2020-05-02 21:49:19 +0900

    Create test3.js

---
+++ scratch3/test3.js
@@ -0,0 +1,566 @@
+class MachineLearningText {
+
+    constructor() {
+        this._labels = {
+            items : [  'good',  'bad',  ],
+            acceptReporters: true
+        };
+
+        this._statuses = [
+            { value : 'Ready', text : 'ready to use' },
+            { value : 'Training', text : 'still training' },
+            { value : 'Model Failed', text : 'ERROR - broken' },
+            { value : 'Model Non Existent', text : 'ERROR - non-existent' }
+        ];
+
+
+    }
+
+
+    getInfo() {
+
+        return {
+            // making the id unique to allow multiple instances
+            id: 'mlforkidstext115b0af08c6b11ea92f8f7a0b900a81b',
+            // the name of the student project
+            name: 'bb',
+
+            // colour for the blocks
+            color1: '#4B4A60',
+            // colour for the menus in the blocks
+            color2: '#707070',
+            // border for blocks and parameter gaps
+            color3: '#4c97ff',
+
+            // Machine Learning for Kids site icon
+//            menuIconURI: this._icon,
+//            blockIconURI: this._icon,
+
+            // blocks to add to the new section
+            blocks: [
+                // classify the text and return the label
+                {
+                    opcode: 'label',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: {
+                        default: 'recognise text [TEXT] (label)',
+                        id: 'mlforkids.text.recogniseLabel'
+                    },
+                    arguments: {
+                        TEXT: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'text'
+                        }
+                    }
+                },
+
+                // classify the text and return the confidence score
+                {
+                    opcode: 'confidence',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: {
+                        default: 'recognise text [TEXT] (confidence)',
+                        id: 'mlforkids.text.recogniseConfidence'
+                    },
+                    arguments: {
+                        TEXT: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'text'
+                        }
+                    }
+                },
+
+                // provide blocks representing each of the labels
+                {
+                    opcode: 'return_label_0',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: ' good'
+                },
+                {
+                    opcode: 'return_label_1',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: ' bad'
+                },
+
+                // add training data to the project
+                {
+                    opcode: 'addTraining',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: {
+                        default: 'add training data [TEXT] [LABEL]',
+                        id: 'mlforkids.text.addTraining'
+                    },
+                    arguments: {
+                        TEXT: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'text'
+                        },
+                        LABEL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: this._labels.items[0],
+                            menu: 'labels'
+                        }
+                    }
+                },
+
+                // train a new machine learning model
+                {
+                    opcode: 'trainNewModel',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: {
+                        default: 'train new machine learning model',
+                        id: 'mlforkids.text.trainNewModel'
+                    }
+                },
+
+                // get the status of the machine learning model
+                {
+                    opcode: 'checkModelStatus',
+                    blockType: Scratch.BlockType.BOOLEAN,
+                    text: {
+                        default: 'Is the machine learning model [STATUS] ?',
+                        id: 'mlforkids.text.checkModelStatus'
+                    },
+                    arguments: {
+                        STATUS: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: this._statuses[0].value,
+                            menu: 'statuses'
+                        }
+                    }
+                }
+            ],
+
+            menus: {
+                labels: this._labels,
+                statuses: this._statuses
+            }
+        };
+    }
+
+
+    label({ TEXT }) {
+        return new Promise(resolve => getTextClassificationResponse(TEXT, TEXT, 'class_name', resolve));
+    }
+    confidence({ TEXT }) {
+        return new Promise(resolve => getTextClassificationResponse(TEXT, TEXT, 'confidence', resolve));
+    }
+
+
+    return_label_0 () {
+        return 'good';
+    }
+    return_label_1 () {
+        return 'bad';
+    }
+
+
+    addTraining({ TEXT, LABEL }) {
+        var txt = cleanUpText(TEXT, 1024);
+
+        var url = new URL('https://machinelearningforkids.co.uk/api/scratch/3242f660-8c6b-11ea-92f8-f7a0b900a81b6ab70d74-dba7-4353-ac40-3a1aa815a364/train');
+
+        var options = {
+            headers : {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-User-Agent': 'mlforkids-scratch3-text'
+            },
+            method : 'POST',
+            body : JSON.stringify({
+                data : txt,
+                label : LABEL
+            })
+        };
+
+        return fetch(url, options)
+            .then((response) => {
+                if (response.status !== 200) {
+                    return response.json();
+                }
+            })
+            .then((responseJson) => {
+                if (responseJson) {
+                    console.log(responseJson);
+                }
+            });
+    }
+
+
+    trainNewModel() {
+        if (trainingModel || // currently submitting a new-model request, OR
+            lastModelTrainedRecently()) // we very recently submitted one
+        {
+            console.log('ignoring request - last status:');
+            console.log(classifierStatus);
+            return;
+        }
+
+        return trainNewClassifier()
+            .then((responseJson) => {
+                if (responseJson) {
+                    console.log(responseJson);
+                }
+            });
+    }
+
+
+    checkModelStatus({ STATUS }) {
+        return getStatus()
+            .then((statusObj) => {
+                switch(STATUS) {
+                    case 'Ready':
+                        return statusObj.status === STATUS_OK;
+                    case 'Training':
+                        return statusObj.status === STATUS_WARNING;
+                    default:
+                        return statusObj.status === STATUS_ERROR &&
+                               statusObj.msg === STATUS;
+                }
+            });
+    }
+}
+
+
+
+var resultsCache = {
+    // schema for objects in this cache:
+    //
+    // cacheKey (text to be classified) : {
+    //
+    //    // returned by the API
+    //    class_name : topClassName,
+    //    confidence : topClassConfidence,
+    //    classifierTimestamp : isoStringDateTime,
+    //
+    //    // added locally
+    //    fetched : timestamp-ms-since-epoch
+    // }
+};
+
+
+var STATUS_ERROR = 0;
+var STATUS_WARNING = 1;
+var STATUS_OK = 2;
+
+
+var classifierStatus = {
+    status : STATUS_WARNING,
+    msg : 'Getting status',
+};
+
+var TEN_SECONDS = 10 * 1000;
+var TWENTY_SECONDS = 20 * 1000;
+var THIRTY_SECONDS = 30 * 1000;
+var ONE_MINUTE = 60 * 1000;
+var TWO_MINUTES = 2 * 60 * 1000;
+var FIVE_MINUTES = 5 * 60 * 1000;
+
+
+// the last time that we used the API to check the status
+//  of the ML model
+var lastStatusCheck = 0;
+// the last time that we used the API to train a new ML model
+var lastModelTrain = 0;
+
+// returns true if the last time that we checked the ML model
+//  status was longer ago than the provided time limit
+function lastStatusCheckExceededLimit(limit) {
+    return (Date.now() - lastStatusCheck) > limit;
+}
+// returns true if the last time that we trained a ML model
+//  was too recently to do again
+function lastModelTrainedRecently() {
+    return (lastModelTrain + ONE_MINUTE) > Date.now();
+}
+
+// returns the current date in the format that the API uses
+function nowAsString() {
+    return new Date().toISOString();
+}
+// returns true if the provided timestamp is within the last 10 seconds
+function veryRecently(timestamp) {
+    return (timestamp + TEN_SECONDS) > Date.now();
+}
+
+
+
+
+
+// Submit xhr request to the classify API to get a label for a string
+function classifyText(text, cacheKey, lastmodified, callback) {
+    var url = new URL('https://machinelearningforkids.co.uk/api/scratch/3242f660-8c6b-11ea-92f8-f7a0b900a81b6ab70d74-dba7-4353-ac40-3a1aa815a364/classify');
+    url.searchParams.append('data', text);
+
+    var options = {
+        headers : {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-User-Agent': 'mlforkids-scratch3-text',
+
+            'If-Modified-Since': lastmodified
+        }
+    };
+
+    return fetch(url, options)
+        .then((response) => {
+            if (response.status === 304 && resultsCache[cacheKey]) {
+                // the API returned NOT-MODIFIED so we'll
+                // reuse the value we got last time
+                callback(resultsCache[cacheKey]);
+            }
+            else if (response.status === 200) {
+                response.json().then((responseJson) => {
+                    if (responseJson && responseJson.length > 0) {
+                        // we got a result from the classifier
+                        callback(responseJson[0]);
+                    }
+                    else {
+                        callback({
+                            class_name: 'Unknown',
+                            confidence: 0,
+                            classifierTimestamp: nowAsString()
+                        });
+                    }
+                });
+            }
+            else {
+                console.log(response);
+
+                callback({
+                    class_name: 'Unknown',
+                    confidence: 0,
+                    classifierTimestamp: nowAsString()
+                });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+
+            callback({
+                class_name: 'Unknown',
+                confidence: 0,
+                classifierTimestamp: nowAsString()
+            });
+        });
+}
+
+
+function getTextClassificationResponse(text, cacheKey, valueToReturn, callback) {
+    var cleanedUpText = cleanUpText(text, 2000);
+    if (!cleanedUpText) {
+        return callback('You need to put some text that you want to classify in here');
+    }
+
+    var cached = resultsCache[cacheKey];
+
+    // protect against kids putting the ML block inside a forever
+    //  loop making too many requests too quickly
+    // this throttling means we won't try and classify the same
+    //  string more than once every 10 seconds
+    if (cached && cached.fetched && veryRecently(cached.fetched)) {
+        return callback(cached[valueToReturn]);
+    }
+
+    // if we have a cached value, get it's timestamp
+    var lastmodified = nowAsString();
+    if (cached && cached.classifierTimestamp) {
+        lastmodified = cached.classifierTimestamp;
+    }
+
+    // submit to the classify API
+    classifyText(cleanedUpText, cacheKey, lastmodified, function (result) {
+        if (result.random) {
+            // We got a randomly selected result (which means we must not
+            //  have a working classifier).
+            console.log('randomly selected result returned by API');
+        }
+
+        // update the timestamp to allow local throttling
+        result.fetched = Date.now();
+
+        // cache the result to let it be used again
+        resultsCache[cacheKey] = result;
+
+        // return the requested value from the response
+        callback(result[valueToReturn]);
+    });
+}
+
+
+// are we currently checking the classifier status?
+//  used as a primitive lock to prevent multiple concurrent checks being made
+var checkingStatus = false;
+// are we currently training a classifier?
+//  used as a primitive lock to prevent multiple concurrent requests being made
+var trainingModel = false;
+
+
+
+// make an XHR request to train a new ML model
+function trainNewClassifier() {
+    trainingModel = true;
+    lastStatusCheck = Date.now();
+    lastModelTrain = Date.now();
+
+    var url = new URL('https://machinelearningforkids.co.uk/api/scratch/3242f660-8c6b-11ea-92f8-f7a0b900a81b6ab70d74-dba7-4353-ac40-3a1aa815a364/models');
+    var options = {
+        headers : {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-User-Agent': 'mlforkids-scratch3-text'
+        },
+        method : 'POST'
+    };
+
+    return fetch(url, options)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json().then((responseJson) => {
+                    classifierStatus = responseJson;
+                });
+            }
+            else {
+                console.log(response);
+
+                classifierStatus = {
+                    status : STATUS_ERROR,
+                    msg : 'Unable to communicate with machine learning service'
+                };
+            }
+        })
+        .then(() => {
+            trainingModel = false;
+            return classifierStatus;
+        })
+        .catch((err) => {
+            console.log(err);
+            trainingModel = false;
+
+            classifierStatus = {
+                status : STATUS_ERROR,
+                msg : 'Unable to communicate with machine learning service'
+            };
+            return classifierStatus;
+        });
+}
+
+
+// make an XHR request to fetch the current status of the ML model
+function fetchStatus() {
+    checkingStatus = true;
+    lastStatusCheck = Date.now();
+
+    var url = new URL('https://machinelearningforkids.co.uk/api/scratch/3242f660-8c6b-11ea-92f8-f7a0b900a81b6ab70d74-dba7-4353-ac40-3a1aa815a364/status');
+    var options = {
+        headers : {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-User-Agent': 'mlforkids-scratch3-text'
+        }
+    };
+
+    return fetch(url, options)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json().then((responseJson) => {
+                    classifierStatus = responseJson;
+                });
+            }
+            else {
+                console.log(response);
+
+                classifierStatus = {
+                    status : STATUS_ERROR,
+                    msg : 'Unable to communicate with machine learning service'
+                };
+            }
+        })
+        .then(() => {
+            checkingStatus = false;
+            return classifierStatus;
+        })
+        .catch((err) => {
+            console.log(err);
+            checkingStatus = false;
+
+            classifierStatus = {
+                status : STATUS_ERROR,
+                msg : 'Unable to communicate with machine learning service'
+            };
+            return classifierStatus;
+        });
+}
+
+
+
+// Get the current status of the ML model
+//  This will decide whether it is worth calling the status API
+//  or whether it is okay to re-use a previously cached check
+//  Either way, it will return a promise.
+function getStatus() {
+
+    // note - the nested if's could be more efficiently written,
+    //  but I'm keeping it this way to make it more readable
+
+    if (checkingStatus) {
+        // already have a check in-flight, so we don't
+        // start another
+        return Promise.resolve(classifierStatus);
+    }
+    else if (classifierStatus.status === STATUS_OK) {
+        // everything looks okay, so ok to use the cache if
+        // we've checked in the last two minutes
+        if (lastStatusCheckExceededLimit(TWO_MINUTES)) {
+            // get the current status
+            return fetchStatus();
+        }
+        else {
+            return Promise.resolve(classifierStatus);
+        }
+    }
+    else if (classifierStatus.status === STATUS_WARNING) {
+        // looks like the ML model is currently training,
+        //  so we want to check more frequently so we
+        //  know when it's ready to use
+        if (lastStatusCheckExceededLimit(TWENTY_SECONDS)) {
+            // get the current status
+            return fetchStatus();
+        }
+        else {
+            return Promise.resolve(classifierStatus);
+        }
+    }
+    else {
+        // The ML model is broken
+        // This is very unlikely to fix itself, so there
+        // is not much point in aggressively polling
+        if (lastStatusCheckExceededLimit(FIVE_MINUTES)) {
+            // get the current status
+            return fetchStatus();
+        }
+        else {
+            return Promise.resolve(classifierStatus);
+        }
+    }
+}
+
+
+var LINE_BREAKS = /(\r\n|\n|\r|\t)/gm;
+function cleanUpText(str, maxlength) {
+    if (str) {
+        // Newlines in text will cause errors in Watson Assistant API calls
+        // so we replace them a with a space
+        return str.replace(LINE_BREAKS, ' ')
+                .trim()
+                // Protect against text that will exceed the limit on
+                //  number of characters allowed by the API
+                .substr(0, maxlength);
+    }
+    else {
+        return str;
+    }
+}
+
+
+Scratch.extensions.register(new MachineLearningText());
